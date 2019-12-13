c3fc60c06a1fa3baddfa286076404c5fc88338ed
Pierre-Yves BOISBUNON <boisbu@gmail.com>
2019-12-13 13:56:40 +0100

    Add rest api documentation generation

--- lgbsttracker/server/__init__.py
+++ lgbsttracker/server/__init__.py
@@ -1,10 +1,21 @@
 from sanic import Sanic
+from sanic.response import file
 from lgbsttracker.server.handlers import get_endpoints
 
 app = Sanic(__name__)
 
 for http_path, handler, methods in get_endpoints():
     app.add_route(uri=http_path, handler=handler, methods=methods)
+
+
+@app.route("/swagger.json")
+def home(response):
+    return file("./lgbsttracker/server/doc/swagger.json")
+
+
+@app.route("/")
+def home(response):
+    return file("./lgbsttracker/server/doc/index.html")
 
 
 def _run_server():
